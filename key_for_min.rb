cab6ce9279a6a312d60193fc32b3b5f54050d3f4
Brandon  Harris <cheeridon@gmail.com>
2017-10-02 19:55:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_key = ""
   lowest_num = numbers[0]
   name_hash.each do|name, num|
-    if lowest_num > num
+    if num < lowest_num
       lowest_key = name
       lowest_num = num
     end
