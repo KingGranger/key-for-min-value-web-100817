668f429cb911b9a2026890be17b7477264b3838a
Brandon  Harris <cheeridon@gmail.com>
2017-10-02 19:39:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,7 @@
 
 def key_for_min_value(name_hash)
   numbers = name_hash.collect {|name, number| number}
+  lowest_num = numbers[0]
+  numbers.each{|num| lowest_num = num if num < lowest_num}
+  lowest_num
 end
